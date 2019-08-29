631008a37adf5f1407e2da01e3435d1155b2c683
Cealestus <meisler@live.ca>
2019-08-28 22:40:40 -0600

    Calculator handles custom delimiters
     - Custom delimiters are used when the numbers string begins with '//' and will be parsed until '\n'
     - Calling with a negative number will raise a SyntaxError and include the invalid values
     - testCalculator tests the above test cases

--- stringCalculator.py
+++ stringCalculator.py
@@ -6,6 +6,14 @@ class Calculator:
         if not numbers:
             return 0
         else:
+            delimiter = ','
+            if numbers[0:2] == "//":
+                split_numbers = numbers.split('\n', 1)
+                delimiter = split_numbers[0][2:]
+                numbers = split_numbers[1]
             numbers = numbers.replace('\n', '')
-            values = list(map(int, numbers.split(',')))
+            values = list(map(int, numbers.split(delimiter)))
+            negatives = [i for i in values if i < 0]
+            if negatives:
+                raise SyntaxError("Negatives not allowed: " + str(negatives))
             return sum(values)
--- testCalculator.py
+++ testCalculator.py
@@ -1,5 +1,6 @@
 from stringCalculator import Calculator
 
+SyntaxErrorString= "SyntaxError"
 testValues = [
     ["1,2,5", 8],
     ["", 0],
@@ -7,14 +8,27 @@ testValues = [
     ["0", 0],
     ["100, 101, 5", 206],
     ["1\n,2,3", 6],
-    ["1,\n2,4", 7]
+    ["1,\n2,4", 7],
+    ["//;\n1;3;4", 8],
+    ["//$\n1$2$3", 6],
+    ["//@\n2@3@8", 13],
+    ["//@@\n2@@5@@12", 19],
+    ["-1,5", SyntaxErrorString],
+    ["5,10,-12,-100", SyntaxErrorString],
+    ["//%\n-5%6%12", SyntaxErrorString]
 ]
 
 calculator = Calculator()
 
 for pairs in testValues:
-    total = calculator.add(pairs[0])
-    if total == pairs[1]:
-        print("Add succeeded for input: ", pairs[0], " result: ", str(total))
-    else:
-        print("Add should have equalled ", str(pairs[1]), " for input: ", pairs[0], "returned: ", total)
+    try:
+        total = calculator.add(pairs[0])
+        if total == pairs[1]:
+            print("Add succeeded for test: ", testValues.index(pairs), "result: ", total)
+        else:
+            print("Add should have equalled ", str(pairs[1]), " for test: ", testValues.index(pairs), "returned: ", total)
+    except SyntaxError as value:
+        if pairs[1] == "SyntaxError":
+            print("Add succeeded for test: ", testValues.index(pairs), "result: SyntaxError - ", value)
+        else:
+            print("Add threw SyntaxError when it should not have for test: ", testValues.index(pairs))
